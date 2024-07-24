bf84b55f4953f479e3e0daa68e5bffce75ef37da
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-07-24 05:51:13 -0700

    logical or AND

--- src/IfThen.java
+++ src/IfThen.java
@@ -2,7 +2,12 @@ public class IfThen {
     public static void main(String[] args) {
         boolean isAlien = false;
         if (!isAlien){
-            System.out.print("It Is Not Alien");
+            System.out.println("It Is Not Alien");
+        }
+        int secondTopScore = 20;
+        int topScore = 60;
+        if((topScore >secondTopScore) && (topScore < 100) ){
+            System.out.print("Greater Than second top score and less than 100");
         }
     }
 }
