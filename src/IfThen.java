f29c74e217a8b9dda64ac7af076a6a6e43af1a73
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-07-24 05:24:40 -0700

    if Statment

---
+++ src/IfThen.java
@@ -0,0 +1,8 @@
+public class IfThen {
+    public static void main(String[] args) {
+        boolean isAlien = false;
+        if (!isAlien){
+            System.out.print("It Is Not Alien");
+        }
+    }
+}
