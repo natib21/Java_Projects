4f6efb602e03b8da044ff688a2a5ebc15c9ae56b
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-07-25 07:32:37 -0700

    Methods in Java

---
+++ src/Methods.java
@@ -0,0 +1,20 @@
+public class Methods {
+    public static void main(String[] args) {
+      int highScore = calculateScore(true,800,5,100);
+      System.out.println("The High Score "+highScore);
+      System.out.println("The Next highScore Is "+
+      calculateScore(true,10000,8 , 200));
+
+    }
+    public static int calculateScore(boolean gameOver,int score , int levelComplete, int bonus){
+
+        int finalScore = score;
+
+        if (gameOver){
+            finalScore += (levelComplete * bonus);
+            finalScore += 1000;
+
+        }
+        return finalScore;
+    }
+}
