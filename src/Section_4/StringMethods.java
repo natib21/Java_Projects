5b8b67de76d0aa8ed67c66b8070b54696dc5dead
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-08-17 01:30:50 -0700

    String Manipulation

---
+++ src/Section_4/StringMethods.java
@@ -0,0 +1,17 @@
+package Section_4;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        String birthDate = "25/11/1982";
+        int startingIndex = birthDate.indexOf("1982");
+        System.out.println("startingIndex = "+startingIndex);
+        System.out.println("Birth year = "+birthDate.substring(startingIndex));
+        System.out.println("Month = "+ birthDate.substring(3,5));
+
+
+        String newDate = String.join("/","25","11","1982");
+       System.out.println(newDate);
+
+       System.out.println(newDate.replace('/','-'));
+    }
+}
