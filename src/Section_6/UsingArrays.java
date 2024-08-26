50e6c4d9e46590015753f4bde180b82d9865fc71
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-08-26 07:18:20 -0700

    Array Methods copyof,equal,binarySearch,sort,toString,fill

--- src/Section_6/UsingArrays.java
+++ src/Section_6/UsingArrays.java
@@ -19,6 +19,22 @@ public class UsingArrays {
    Arrays.sort(fourthArray);
         System.out.println(Arrays.toString(thirdArray));
         System.out.println(Arrays.toString(fourthArray));
+
+   String[] sArray = {"Able","Jane","Mark","Ralph","David"};
+    Arrays.sort(sArray);
+    System.out.println(Arrays.toString(sArray));
+    if(Arrays.binarySearch(sArray,"Mark") >= 0){
+        System.out.println("Found Mark In The List");
+    }
+
+    int[] s1 ={1,2,3,4,5};
+    int[] s2 = {1,2,3,4,5};
+
+    if(Arrays.equals(s1,s2)){
+        System.out.println("Arrays are equal");
+    }
+
+
     }
     private static int[] getRandomArray(int len){
         Random random = new Random();
