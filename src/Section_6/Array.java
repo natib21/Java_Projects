a7e51fa7591bb8d87eaa2f018897054325d6ddce
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-08-26 06:41:16 -0700

    more on arrays

--- src/Section_6/Array.java
+++ src/Section_6/Array.java
@@ -1,5 +1,7 @@
 package Section_6;
 
+import java.util.Arrays;
+
 public class Array {
     public static void main(String[] args) {
         int[] myIntArray = new int[]{1,2,3,4,5};
@@ -14,5 +16,22 @@ public class Array {
         int arrayLength = firsTen.length;
         System.out.println("Length Of array = "+arrayLength);
         System.out.println("last = "+firsTen[arrayLength - 1]);
+
+        int[] newArray;
+        newArray = new int[5];
+        for(int i=0;i<newArray.length;i++){
+            newArray[i] = newArray.length - i;
+            System.out.print(newArray[i]+ " " );
+        }
+        System.out.println(" ");
+        for(int e : newArray){
+            System.out.print(e + " ");
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(newArray));
+        Object objectVariable = newArray;
+        if(objectVariable instanceof int[]){
+            System.out.println("ObjectVariable is really an int array");
+        }
     }
 }
