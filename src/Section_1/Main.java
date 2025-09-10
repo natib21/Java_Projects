35f4ab842c97dc9fcb265ec75552366385b6bb74
Nathnael <nathnaelzelalem@gmail.com>
2025-09-10 12:07:50 +0300

    Find the smallest value

--- src/Section_1/Main.java
+++ src/Section_1/Main.java
@@ -2,6 +2,15 @@ package Section_1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+     printYearsAndDays(525600);
+    }
+    public static void printYearsAndDays(long minutes){
+        if(minutes <0){
+            System.out.print("Invalid Value");
+            return;
+        }
+       long hour = minutes / 60;
+        long day = minutes % 60;
+        System.out.println(hour + ":" + day);
     }
 }
--- src/Section_6/MinimumElementChallenge/Main.java
+++ src/Section_6/MinimumElementChallenge/Main.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-     ;
-
-         System.out.println(readIntegers());
+     int [] val = readIntegers();
+     System.out.println(Arrays.toString(val));
+     int minVal = findMin(val);
+     System.out.println(minVal);
     }
-    public static   int[] readIntegers(){
+    public static  int[] readIntegers(){
         Scanner s = new Scanner(System.in);
+        System.out.print("Enter a list of integers, separated by commas: ");
         String num = s.nextLine();
         String[] splits = num.split(",");
         int[] val = new int[splits.length];
@@ -18,6 +20,15 @@ public class Main {
             val[i] = Integer.parseInt(splits[i].trim());
         }
         return val;
+    }
+    public static int findMin(int[] val){
+        int min = val[0];
+        for(int i = 1; i < val.length; i++){
+            if(val[i] < min){
+                min = val[i];
+            }
 
+        }
+        return min;
     }
 }
