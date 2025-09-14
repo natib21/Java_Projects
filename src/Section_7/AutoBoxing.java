973df8c04af1a4b4637c119c7d17357e66b72ebd
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2025-09-14 12:43:39 +0300

    Autoboxing and Unboxing with List like ArrayList

--- src/Section_7/AutoBoxing.java
+++ src/Section_7/AutoBoxing.java
@@ -1,5 +1,7 @@
 package Section_7;
 
+import java.util.Arrays;
+import java.util.ArrayList;
 public class AutoBoxing {
     public static void main(String[] args) {
         Integer boxedInt = Integer.valueOf(15); // preferred but unnecessary
@@ -14,6 +16,29 @@ public class AutoBoxing {
       double resultUnboxing = getDoubleObject();
 
 //      System.out.println(autoUnboxed.getClass().getName());
+
+        Integer[] boxInt = new Integer[5];
+        boxInt[0] = 50;
+        System.out.println(boxInt.getClass().getName());
+        Character[] name = {'a','b','c','d'};
+        System.out.println(Arrays.toString(name));
+
+        var outList = gotList(1,2,3,4,5);
+        System.out.println(outList);
+    }
+
+    private static ArrayList<Integer> gotList(int... varargs){
+        ArrayList<Integer> lists = new ArrayList<>();
+        for(int i : varargs){
+            lists.add(i);
+        }
+        return lists;
+    }
+    private static int returnAnInt(Integer val) {
+        return val;
+    }
+    private static Integer returnAnInt(int val){
+        return val;
     }
     private static Double getDoubleObject(){
         return Double.valueOf(15);
